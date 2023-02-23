e7bc0b0eef0f65c85ea5041014173cb0efd4146f
lalitha <lalitha.mycareer@gmail.com>
2023-02-23 14:39:23 +0530

    new project

--- branchone.php
+++
@@ -1,4 +0,0 @@
-<h1>This is Branchone file</h1>
-<h1>
-	Edited from branch three
-</h1>
--- index.php
+++ index.php
@@ -3,12 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>title master/one</title>
+	<title>Vue Project changed</title>
 </head>
 <body>
-	<h1>Master edit</h1>
-	<h1>Branchfour edit</h1>
-	<h1>Branchthree</h1>
-	<h1>Branchfive</h1>
 </body>
 </html>
